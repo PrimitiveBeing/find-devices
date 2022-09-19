0128ec2a11a79202d61d1beb465b7dbf6ef66194
PrimitiveBeing <113930934+PrimitiveBeing@users.noreply.github.com>
2022-09-19 17:07:56 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+p () {
+ip addr | grep /24 | cut  -d "." -f 3
+}
+cut="$(p)"
+
+for (( c=1; c<=225; c++ ))
+  do
+    if  ping -b -c 1 -s 1 192.168.$cut.$c &>/dev/null
+    then
+      echo "DEVICE FOUND 192.168.$cut.$c"
+    else
+      echo "NOT FOUND $c"
+    fi
+  done
